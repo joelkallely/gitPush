35c8a7cdecabf289c2789214aacac140c4e9ee1f
Joel Jose <joel.jose@flytxt.com>
2018-06-22 11:28:40 +0530

    initial commit

---
+++ src/main/java/jj/gitPush/App.java
@@ -0,0 +1,13 @@
+package jj.gitPush;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
